956b80fb99170cfb5a2ddbb50004d2bdbe3cb8f5
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-03-04 14:31:44 +0530

    Updated Seeders

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
@@ -14,6 +16,23 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+
+        // create permissions
+        Permission::create(['name' => 'driver_crud']);
+        Permission::create(['name' => 'transporter_crud']);
+        Permission::create(['name' => 'vehicle_crud']);
+        Permission::create(['name' => 'client_crud']);
+
+        // create director roles and assign permissions
+        $role = Role::create(['name' => 'admin']);
+        $role->givePermissionTo([
+            'driver_crud',
+            'transporter_crud',
+            'vehicle_crud',
+            'client_read',
+        ]);
     }
 }
