1535ed864ec65dfa13a277d4c85cee89d44d4882
tek2991 <eliezer.kt@gmail.com>
2022-03-10 07:48:40 +0530

    Added RawDataImport migration and model

---
+++ app/Models/RawDataImport.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RawDataImport extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'log_sheet',
+        'date',
+        'invoice_no',
+        'invoice_date',
+        'payer',
+        'payer_name',
+        'gross_weight',
+        'transporter_name',
+        'container_id',
+        'destination',
+        'no_of_packs',
+        'driver_no',
+        'is_processed',
+    ];
+}
---
+++ database/migrations/2022_03_10_020038_create_table_raw_data_imports.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_raw_data_imports', function (Blueprint $table) {
+            $table->id();
+            $table->string('log_sheet')->nullable();
+            $table->string('date')->nullable();
+            $table->string('invoice_no')->nullable();
+            $table->string('invoice_date')->nullable();
+            $table->string('payer')->nullable();
+            $table->string('payer_name')->nullable();
+            $table->string('gross_weight')->nullable();
+            $table->string('transporter_name')->nullable();
+            $table->string('container_id')->nullable();
+            $table->string('destination')->nullable();
+            $table->string('no_of_packs')->nullable();
+            $table->string('driver_no')->nullable();
+            $table->boolean('is_processed')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_raw_data_imports');
+    }
+};
