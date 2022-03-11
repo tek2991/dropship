8e5b599d3b7bcccbefd1c122c27d2df3d48bb0de
tek2991 <eliezer.kt@gmail.com>
2022-03-11 08:33:59 +0530

    Removed dead code

--- app/Http/Controllers/Admin/LogSheetController.php
+++ app/Http/Controllers/Admin/LogSheetController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\LogSheet;
-use Illuminate\Http\Request;
 
 class LogSheetController extends Controller
 {
@@ -22,26 +21,6 @@ class LogSheetController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -55,39 +34,5 @@ class LogSheetController extends Controller
             'logSheet' => $logSheet->load('driverUser', 'transporterUser', 'vehicle'),
             'invoices' => $logSheet->invoices()->with('clientUser', 'client')->paginate(),
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\LogSheet  $logSheet
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(LogSheet $logSheet)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\LogSheet  $logSheet
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, LogSheet $logSheet)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\LogSheet  $logSheet
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(LogSheet $logSheet)
-    {
-        //
     }
 }
