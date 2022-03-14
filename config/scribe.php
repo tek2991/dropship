577243f961dabe584976b319504bf982d563d3cc
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-03-14 13:13:39 +0530

    Added gitignore for scribe blade file

--- config/scribe.php
+++ config/scribe.php
@@ -19,7 +19,7 @@ return [
     /*
      * The base URL displayed in the docs. If this is empty, Scribe will use the value of config('app.url').
      */
-    'base_url' => 'http://dropship.webrefiner.com',
+    'base_url' => null,
 
     /*
      * Tell Scribe what routes to generate documentation for.
