43b588b1ae2450381cd9e362e19197895aa14e64
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-07-20 05:41:11 +0530

    Bug fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @if (auth()->user()->hasRole('admin'))
+                    @if (auth()->user()->hasRole('admin') || auth()->user()->hasRole('manager'))
                         @php
                             $counts = [
                                 'drivers' => [
