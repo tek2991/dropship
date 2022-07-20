6ebf587daeaeeb3980562aed7060cde4dc000f1e
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-07-20 05:39:12 +0530

    Bug fix

--- resources/views/admin/locations/index.blade.php
+++ resources/views/admin/locations/index.blade.php
@@ -4,13 +4,15 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Locations') }}
             </h2>
-            <x-button-link href="{{ route('admin.locations.create') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                </svg>
-                <span>Create</span>
-            </x-button-link>
+            @if (Auth::user()->isAdmin())
+                <x-button-link href="{{ route('admin.locations.create') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                    </svg>
+                    <span>Create</span>
+                </x-button-link>
+            @endif
         </span>
     </x-slot>
 
@@ -19,7 +21,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-success-message />
-                    <livewire:location-table/>
+                    <livewire:location-table />
                 </div>
             </div>
         </div>
