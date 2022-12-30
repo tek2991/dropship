02c02f095f975f917fb923075f2b35cb60de0222
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-12-30 16:39:49 +0530

    Added pendancy tqable

---
+++ app/Http/Livewire/PendingInvoiceTable.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\PowerGrid;
+use PowerComponents\LivewirePowerGrid\PowerGridEloquent;
+use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\Traits\ActionButton;
+
+final class PendingInvoiceTable extends PowerGridComponent
+{
+    use ActionButton;
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Datasource
+    |--------------------------------------------------------------------------
+    | Provides data to your Table using a Model or Collection
+    |
+    */
+    public function datasource(): ?Collection
+    {
+        $query = 'select date as id,
+            count(case when delivery_state_id = 1 then id end) as Pending,
+            count(case when delivery_state_id = 2 then id end) as Delivered,
+            count(case when delivery_state_id = 3 then id end) as Cancelled,
+            count(id) as Total 
+            from invoices
+            group by date';
+
+        $data = collect(\DB::select($query));
+
+        return $data;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Relationship Search
+    |--------------------------------------------------------------------------
+    | Configure here relationships to be used by the Search and Table Filters.
+    |
+    */
+    public function setUp(): void
+    {
+        $this->showCheckBox()
+            ->showPerPage()
+            ->showExportOption('download', ['excel', 'csv'])
+            ->showSearchInput();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Add Column
+    |--------------------------------------------------------------------------
+    | Make Datasource fields available to be used as columns.
+    | You can pass a closure to transform/modify the data.
+    |
+    */
+    public function addColumns(): ?PowerGridEloquent
+    {
+        return PowerGrid::eloquent()
+            ->addColumn('id')
+            ->addColumn('Pending')
+            ->addColumn('Delivered')
+            ->addColumn('Cancelled')
+            ->addColumn('Total');
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Include Columns
+    |--------------------------------------------------------------------------
+    | Include the columns added columns, making them visible on the Table.
+    | Each column can be configured with properties, filters, actions...
+    |
+
+    */
+     /**
+     * PowerGrid Columns.
+     *
+     * @return array<int, Column>
+     */
+    public function columns(): array
+    {
+        return [
+            Column::add()
+                ->title('DATE')
+                ->field('id')
+                ->makeInputDatePicker(),
+            Column::add()
+                ->title('PENDING')
+                ->field('Pending')
+                ->sortable(),
+            Column::add()
+                ->title('DELIVERED')
+                ->field('Delivered')
+                ->sortable(),
+            Column::add()
+                ->title('CANCELLED')
+                ->field('Cancelled')
+                ->sortable(),
+            Column::add()
+                ->title('TOTAL')
+                ->field('Total')
+                ->sortable(),
+        ];
+    }
+}
